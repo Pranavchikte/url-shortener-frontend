9455f2c74dfa19cc13a908d84614f43f08786d6b
Pranavchikte <chiktepranav1378@gmail.com>
2025-10-14 00:21:31 +0530

    fix: Allow className prop on Menu component to resolve build error

--- components/ui/navbar-menu.tsx
+++ components/ui/navbar-menu.tsx
@@ -109,13 +109,17 @@ export const ProductItem = ({
   );
 };
 
-export const HoveredLink = ({ children, ...rest }: any) => {
+export const HoveredLink = ({
+  children,
+  ...rest
+}: React.AnchorHTMLAttributes<HTMLAnchorElement>) => {
   return (
     <a
       {...rest}
-      className="text-neutral-700 dark:text-neutral-200 hover:text-black "
+      className="text-neutral-700 dark:text-neutral-200 hover:text-black"
     >
       {children}
     </a>
   );
 };
+
